8c1b59412f54fbd129a33b88cdb2aa612aaad79b
Hieu Vu <trunghieuubhs@gmail.com>
2023-09-24 00:59:22 +0700

    update ui part 2

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,9 @@
-import { Routes, Route } from "react-router-dom";
+import AppRouter from "./AppRouter";
 
-import Login from "./pages/Login/Login";
-import Home from "./pages/Home/Home";
-import Cart from "./pages/Cart/Cart";
-import Register from "./pages/Register/Register";
 function App() {
   return (
     <>
-      <Routes>
-        <Route path="/" element={<Home />}>
-          <Route path="/login" element={<Login />} />
-          <Route path="/register" element={<Register />} />
-          <Route path="/cart" element={<Cart />} />
-        </Route>
-      </Routes>
+      <AppRouter />
     </>
   );
 }
--- src/AppRouter.tsx
+++ src/AppRouter.tsx
@@ -1,9 +1,24 @@
 import { Routes, Route } from "react-router-dom";
 
+import AuthorizationUserLayout from "./components/layouts/AuthorizationUserLayout";
+import DefaultLayout from "./components/layouts/DefaultLayout";
+import Login from "./pages/Login/Login";
+import Register from "./pages/Register/Register";
+import Cart from "./pages/Cart";
+import Home from "./pages/Home/Home";
+
 export default function AppRouter() {
   return (
     <Routes>
-      <Route></Route>
+      <Route element={<AuthorizationUserLayout />}>
+        <Route path="/cart" element={<Cart />} />
+      </Route>
+      <Route element={<DefaultLayout />}>
+        <Route path="/" element={<Home />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Register />} />
+        <Route path="/aboutUs" />
+      </Route>
     </Routes>
   );
 }
